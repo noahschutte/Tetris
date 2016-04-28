8126a53604c7b3e52d91e8f9e1d07e7ec5c83442
Noah Schutte <noahschutte@gmail.com>
2016-04-28 18:03:03 -0400

    Fix timer

--- public/js/game.js
+++ public/js/game.js
@@ -9,36 +9,32 @@ function Game() {
   this.coreTimer = 0;
   this.timeRunning = 0;
   this.difficultyLevel = 1;
-  this.boardView = new BoardView;
-
 }
+
+var game = new Game();
+
+function startGame() {
+  game.startGameCycle();
+}
+
+Game.prototype.updateTime = function() {
+  $('#timer').html(this.secondsRunning());
+};
 
 Game.prototype.secondsRunning = function() {
   return Math.floor(this.coreTimer / this.frameRate);
 };
 
-function startGame() {
-  var game = new Game;
-}
+Game.prototype.coreGameLoop = function() {
+    this.coreTimer++;
+
+    if (this.coreTimer % 10 === 0) {
+      this.updateTime();
+    }
+};
 
 Game.prototype.startGameCycle = function() {
-  this.coreLoopId = setInterval(this.coreGameLoop.bind(this), 1000 / this.frameRate);
-};
-
-Game.prototype.coreGameLoop = function() {
-  this.coreTimer++;
-
-  if (this.coreTimer % 25 === 0) {
-    this.updateTime();
-  }
-
-  if (this.coreTimer % 3000 === 0) {
-    this.difficultyLevel++;
-  }
-};
-
-Game.prototype.updateTime = function() {
-  View.updateTimer(this.secondsRunning());
+  setInterval(this.coreGameLoop.bind(this), 1000 / this.frameRate);
 };
 
 });
